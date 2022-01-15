b6fbbfe3dcafc9fc15cd4ba744e0f3def656395d
julihcho <juli.h.cho@gmail.com>
2022-01-15 17:46:10 -0500

    add second chart on working mothers

--- data/laborforce/labor-married.py
+++ data/laborforce/labor-married.py
@@ -15,18 +15,17 @@ with open('labor.csv', 'r') as f:
 # print(labor[0].keys())
 #['seriesid', 'Year', 'Period', 'Label', 'Value', '12-Month Net Change', '12-Month % Change', 'Children', 'Gender', 'MaritalStatus']
 
+# 2. Initialize the list which will become the csv for D3
 
-# 2. Headers for the csv that is needed for D3
+headers = ['MaritalStatus', 'Men', 'Women']
+row_names = ['Married, spouse present', 'All other statuses']
 
-headers = ['Marital Status', 'Men', 'Women']
+labor_d3 = [] 
 
-# 3. Create list for the csv that is needed for D3
-labor_married = {}
+labor_d3.append(headers)
 
-for h in headers:
-	labor_married[h] = None
-
-pprint(labor_married)
+for i in row_names:
+	labor_d3.append([i, 0, 0])
 
 # 3. Choose relevant data series
 
@@ -35,59 +34,58 @@ pprint(labor_married)
 # Women, under 18, "Married, spouse present"
 # Women, under 18, "All other statuses"
 
-[
-	{Marriage: something, men: value, women: value}
-	{marriage: something, men: soething, women: something}
-]
-
-checkcount = 0
+count = 0 # double check for duplicates
 
 for row_data in labor:
-	if row_data['Gender'] == "Men" and row_data['Children'] == "under 18" and row_data['MaritalStatus'] == "Married, spouse present" and row_data['Label'] == "2020 Annual":
-		men_married = row_data['Value']
-		checkcount = checkcount+1
-	if row_data['Gender'] == "Men" and row_data['Children'] == "under 18" and row_data['MaritalStatus'] == "All other statuses" and row_data['Label'] == "2020 Annual":
-		men_other = row_data['Value']
-		checkcount = checkcount+1
-	if row_data['Gender'] == "Women" and row_data['Children'] == "under 18" and row_data['MaritalStatus'] == "Married, spouse present" and row_data['Label'] == "2020 Annual":
-		women_married = row_data['Value']
-		checkcount = checkcount+1
-	if row_data['Gender'] == "Women" and row_data['Children'] == "under 18" and row_data['MaritalStatus'] == "All other statuses" and row_data['Label'] == "2020 Annual":
-		women_other = row_data['Value']
-		checkcount = checkcount+1
+	if row_data['Children'] == 'under 18' and row_data['Label'] == '2020 Annual':
 
-labor_married = {'Gender': }
-pprint(checkcount)
+		if row_data['Gender'] == "Men" and row_data['MaritalStatus'] == "Married, spouse present":
+			labor_d3[1][1] = row_data['Value']
+			count = count + 1
 
+		elif row_data['Gender'] == "Women" and row_data['MaritalStatus'] == "Married, spouse present":
+			labor_d3[1][2] = row_data['Value']
+			count = count + 1
+		
+		elif row_data['Gender'] == "Men" and row_data['MaritalStatus'] == "All other statuses":
+			labor_d3[2][1] = row_data['Value']
+			count = count + 1
 
-# # 1. add descriptions of seriesid into the raw data to enable lookup
+		elif row_data['Gender'] == "Women" and row_data['MaritalStatus'] == "All other statuses":
+			labor_d3[2][2] = row_data['Value']
+			count = count + 1
 
-# # read csv files
-# with open('labor-raw.csv', 'r') as f:
-#     reader = csv.DictReader(f)
-#     labor_raw = list(reader)
-
-# with open('labor-id.csv', 'r') as f:
-#     reader = csv.DictReader(f)
-#     labor_id = list(reader)
+# pprint(labor_d3)
+# pprint(count)
 
 
+# 4. convert into dictionary
 
-# # copy raw data into a new list, which will include the information from labor_id
-# labor_raw_id = labor_raw
+ # blank dictionary with only keys 
 
-# # look up the id from labor_raw and add descriptions from labor_id. 
-# for row_data in labor_raw_id:
-# 	for row_id in labor_id:
-# 		if row_id['seriesid'] == row_data['seriesid']:
-# 			row_data['Children'] = row_id['children']
-# 			row_data['Gender'] = row_id['sex']
-# 			row_data['MaritalStatus'] = row_id['maritalstatus']
+# rows = []
+# row = []
 
+# for r in row_names:
+# 	for h in headers:
+# 		if h == headers[0]:
+# 			row.append(r)
+# 		else:
+# 			row.append(None)
+# 	rows.append(row)
+# 	row = []
 
-# # create new csv file with the ids
-# with open('labor.csv', 'w') as f:
-#     writer = csv.writer(f)
-#     writer.writerow(labor_raw_id[0].keys())
-#     for i in labor_raw_id:
-#         writer.writerow(i.values())
+labor_d3_dict = []
+
+for i in labor_d3[1:]:
+	d = {k:v for k,v in zip(headers, i)}
+	labor_d3_dict.append(d)
+
+# pprint(labor_d3_dict)
+
+# 5. create new csv file 
+with open('labor-married.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(labor_d3_dict[0].keys())
+    for i in labor_d3_dict:
+        writer.writerow(i.values())
